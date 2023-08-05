91becfcafd40653b80a157542c0199304e0e0ff7
raihanahnabilah <raihanahnabilah@gmail.com>
2023-08-05 14:19:48 +0800

    add get account and testing

--- service/routes.py
+++ service/routes.py
@@ -69,7 +69,22 @@ def create_accounts():
 ######################################################################
 
 # ... place you code here to READ an account ...
+@app.route("/accounts/<int:account_id>", methods=["GET"])
+def read_account(account_id):
+    """
+    Read a single Account
+    This endpoint will read an Account based on the ID
+    """
+    app.logger.info("Request to read an Account with id: %s", account_id)
+    check_content_type("application/json")
+    account = Account.find(account_id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] cant be found.")
 
+    result = account.serialize()
+    return make_response(
+        jsonify(result), status.HTTP_200_OK
+    )
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
